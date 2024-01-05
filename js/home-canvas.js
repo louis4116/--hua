2ade4a66cc5bf593decbe9438a414360bd80ee1d
Louis <p19970408@gmail.com>
2024-01-05 18:01:42 +0800

    update

--- js/home-canvas.js
+++ js/home-canvas.js
@@ -1,3 +1,5 @@
+const windowWidth = window.innerWidth;
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const yo = document.querySelector(".yo");
@@ -27,12 +29,23 @@ let img3 = new Image();
 let img4 = new Image();
 let img5 = new Image();
 let img6 = new Image();
-img1.src = "./img/home/旋轉背景圖.webp";
-img2.src = "./img/home/big01.webp";
-img3.src = "./img/home/big02.webp";
-img4.src = "./img/home/big03.webp";
-img5.src = "./img/home/big04.webp";
-img6.src = "./img/home/big05.webp";
+if (windowWidth > 500) {
+  backImage.src = "./img/home/旋轉背景圖.jpg";
+  img1.src = "./img/home/旋轉背景圖.webp";
+  img2.src = "./img/home/big01.webp";
+  img3.src = "./img/home/big02.webp";
+  img4.src = "./img/home/big03.webp";
+  img5.src = "./img/home/big04.webp";
+  img6.src = "./img/home/big05.webp";
+} else {
+  backImage.src = "./img/home/phone/ph_01.webp";
+  img1.src = "./img/home/phone/ph_01.webp";
+  img2.src = "./img/home/phone/ph_02.webp";
+  img3.src = "./img/home/phone/ph_03.webp";
+  img4.src = "./img/home/phone/ph_04.webp";
+  img5.src = "./img/home/phone/ph_05.webp";
+  img6.src = "./img/home/phone/ph_01.webp";
+}
 
 const cut2MaskSmall = gsap.timeline({
   scrollTrigger: {
@@ -54,7 +67,7 @@ const cut2PicBigger = gsap.timeline({
     scrub: 0.2,
   },
 });
-const windowWidth = window.innerWidth;
+
 if (windowWidth > 500) {
   backImage.onload = () => {
     // 遮罩顏色;
@@ -121,63 +134,63 @@ if (windowWidth > 500) {
 } else {
   backImage.onload = () => {
     // 遮罩顏色;
-    // ctx.fillStyle = "rgb(193,175,155)";
-    // ctx.save(); // 保存當前狀態
-    // ctx.beginPath();
-    // // ctx.roundRect(pH + pL + squreW2, 0, squreW * 0.6, squreH * 0.6, 20); // 正方形路徑
-    // ctx.roundRect(
-    //   pH + pL + squreW2,
-    //   // squreX + squreW2,
-    //   squreDis + squreH2,
-    //   squreW * 0.6,
-    //   squreH * 0.6,
-    //   20
-    // ); // 正方形路徑
-    // ctx.clip(); // 裁剪路徑
-    // ctx.closePath();
-    // ctx.fill();
-    // ctx.globalCompositeOperation = "source-in";
+    ctx.fillStyle = "rgb(193,175,155)";
+    ctx.save(); // 保存當前狀態
+    ctx.beginPath();
+    // ctx.roundRect(pH + pL + squreW2, 0, squreW * 0.6, squreH * 0.6, 20); // 正方形路徑
+    ctx.roundRect(
+      pH + pL + squreW2,
+      // squreX + squreW2,
+      canvas.height / 2 + squreH2 / 2,
+      squreW * 0.6,
+      squreH * 0.6,
+      20
+    ); // 正方形路徑
+    ctx.clip(); // 裁剪路徑
+    ctx.closePath();
+    ctx.fill();
+    ctx.globalCompositeOperation = "source-in";
     ctx.drawImage(backImage, 0, 0, canvas.width, canvas.height); // 繪製圖片
-    // ctx.restore();
-    // ctx.globalCompositeOperation = "source-over";
+    ctx.restore();
+    ctx.globalCompositeOperation = "source-over";
   };
-  // cut2MaskSmall
-  //   .to(".home-cut-2-fixed-first-squre-wrapper", {
-  //     onUpdate: function () {
-  //       let progress = this.progress();
-  //       let angle = 5 * progress;
-  //       let scaleprogress = Number(((1 - 0.9) * progress).toFixed(3));
-  //       let scaleRes = 1 - scaleprogress;
+  cut2MaskSmall
+    .to(".home-cut-2-fixed-first-squre-wrapper", {
+      onUpdate: function () {
+        let progress = this.progress();
+        let angle = 5 * progress;
+        let scaleprogress = Number(((1 - 0.9) * progress).toFixed(3));
+        let scaleRes = 1 - scaleprogress;
 
-  //       drawMask(scaleRes, angle, img1);
-  //     },
-  //     scale: "0.9",
-  //     rotation: 50,
-  //   })
-  //   .to(".home-cut-2-fixed-first-squre-wrapper", {
-  //     onUpdate: function () {
-  //       let progress = this.progress();
-  //       let scaleprogress = Number(Math.abs((0.9 - 6) * progress).toFixed(3));
-  //       let scaleRes = 0.9 + scaleprogress;
-  //       let img;
-  //       if (scaleRes >= 4.5) {
-  //         img = img5;
-  //       } else if (scaleRes >= 4 && scaleRes < 4.5) {
-  //         img = img2;
-  //       } else if (scaleRes >= 3.25 && scaleRes < 4) {
-  //         img = img3;
-  //       } else if (scaleRes >= 2.5 && scaleRes < 3.25) {
-  //         img = img4;
-  //       } else {
-  //         img = img1;
-  //       }
-  //       drawMask(scaleRes, 5, img);
-  //     },
-  //     scale: "4",
-  //   });
+        drawMask(scaleRes, angle, img1);
+      },
+      scale: "0.9",
+      rotation: 50,
+    })
+    .to(".home-cut-2-fixed-first-squre-wrapper", {
+      onUpdate: function () {
+        let progress = this.progress();
+        let scaleprogress = Number(Math.abs((0.9 - 6) * progress).toFixed(3));
+        let scaleRes = 0.9 + scaleprogress;
+        let img;
+        if (scaleRes >= 4.5) {
+          img = img5;
+        } else if (scaleRes >= 4 && scaleRes < 4.5) {
+          img = img2;
+        } else if (scaleRes >= 3.25 && scaleRes < 4) {
+          img = img3;
+        } else if (scaleRes >= 2.5 && scaleRes < 3.25) {
+          img = img4;
+        } else {
+          img = img1;
+        }
+        drawMask(scaleRes, 5, img);
+      },
+      scale: "4",
+    });
 
-  // cut2PicBigger.to(".yo", { y: "20%", scale: "1.5", ease: "linear" });
-  // cut2MaskSmall.add(cut2PicBigger, "<-0.5");
+  cut2PicBigger.to(".yo", { y: "20%", scale: "1.5", ease: "linear" });
+  cut2MaskSmall.add(cut2PicBigger, "<-0.5");
 }
 
 gsap.registerPlugin(ScrollTrigger);
@@ -187,8 +200,11 @@ gsap.registerPlugin(ScrollSmoother);
 function drawMask(scaleRes, angle, img) {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // 清除 canvas
   ctx.save(); // 保存當前狀態
-
-  ctx.translate(pH + pL + squreW / 2, squreDis + squreH / 2); // 將旋轉中心設為正方形中心
+  if (windowWidth > 500) {
+    ctx.translate(pH + pL + squreW / 2, squreDis + squreH / 2); // 將旋轉中心設為正方形中心
+  } else {
+    ctx.translate(pH + pL + squreW / 2, squreDis + canvas.height / 2 + squreH2); // 將旋轉中心設為正方形中心
+  }
   ctx.rotate((angle * Math.PI) / 180);
   ctx.fillStyle = "rgb(193,175,155)";
   ctx.beginPath();
