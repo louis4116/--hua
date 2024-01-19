eacd90f94b38d8cd8f8c27a5856c5dc2d77123a9
Louis <p19970408@gmail.com>
2024-01-19 18:02:39 +0800

    new

---
+++ js/hot-carousel.js
@@ -0,0 +1,32 @@
+const hotProject = [
+  {
+    title: "HOME FROM HEART",
+    sTitle: "品世界",
+    place: "三重重陽重劃區",
+    para: "品味生活，走遍世界，終於發現原來回家遇見真實的自己，才是真正的「品世界」。",
+    connect: "javascript:;",
+    fb: "javascript:;",
+    mail: "javascript:;",
+    more: "javascript:;",
+  },
+  {
+    title: "HOME FROM HEART",
+    sTitle: "心世界",
+    place: "三重重陽重劃區",
+    para: "品味生活，走遍世界，終於發現原來回家遇見真實的自己，才是真正的「品世界」。",
+    connect: "javascript:;",
+    fb: "javascript:;",
+    mail: "javascript:;",
+    more: "javascript:;",
+  },
+  {
+    title: "HOME FROM HEART",
+    sTitle: "閱世界",
+    place: "三重重陽重劃區",
+    para: "品味生活，走遍世界，終於發現原來回家遇見真實的自己，才是真正的「品世界」。",
+    connect: "javascript:;",
+    fb: "javascript:;",
+    mail: "javascript:;",
+    more: "javascript:;",
+  },
+];
